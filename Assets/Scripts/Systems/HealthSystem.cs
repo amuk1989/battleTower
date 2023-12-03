878a57a73c821102f542f8ac81ac0356b84dc7be
a.muholzoev <amuk89@mail.ru>
2023-12-04 00:31:40 +0700

    add health

---
+++ Assets/Scripts/Systems/HealthSystem.cs
@@ -0,0 +1,20 @@
+using Scellecs.Morpeh;
+using Scellecs.Morpeh.Systems;
+using UnityEngine;
+using Unity.IL2CPP.CompilerServices;
+
+[Il2CppSetOption(Option.NullChecks, false)]
+[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+[Il2CppSetOption(Option.DivideByZeroChecks, false)]
+[CreateAssetMenu(menuName = "ECS/Systems/" + nameof(HealthSystem))]
+public sealed class HealthSystem : UpdateSystem {
+    private Filter _filter;
+    
+    public override void OnAwake()
+    {
+        this._filter = this.World.Filter.With<HealthComponent>().Build();
+    }
+
+    public override void OnUpdate(float deltaTime) {
+    }
+}
